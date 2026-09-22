4bbfedc0af6146fde6e19337b35ed8f70380fb98
iamsweta47-ux <iamsweta47@gmail.com>
2026-09-22 10:42:23 +0530

    Create OverpassApi.kt

---
+++ app/src/main/java/com/pincodehospitalfinder/app/network/OverpassApi.kt
@@ -0,0 +1,22 @@
+package com.pincodehospitalfinder.app.network
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+data class OverpassResponse(
+    val elements: List<OverpassElement>
+)
+
+data class OverpassElement(
+    val id: Long,
+    val lat: Double? = null,
+    val lon: Double? = null,
+    val tags: Map<String, String>? = null
+)
+
+interface OverpassApi {
+    @GET("interpreter")
+    suspend fun getNearbyHospitals(
+        @Query("data") query: String
+    ): OverpassResponse
+}
